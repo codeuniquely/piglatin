1d02ad8cb0791aa40ab9fed6dce928fc577832e9
Steve Saxton <steves@codeuniquely.co.uk>
2017-01-30 05:41:39 +0000

    possible config / options  leave for now

---
+++ src/config.js
@@ -0,0 +1,13 @@
+// ==============================================================
+// Written by steve saxton <steves@codeuniquely.co.uk>
+// various configuration optionsc - not yet used
+// ==============================================================
+
+/* eslint-disable no-unused-labels */
+var config = {
+  maxEntries: 10,
+};
+
+/* eslint-enable no-unused-labels */
+
+export default config;
